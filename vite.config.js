936ecb9bc48eac87e9d80fe6d95e65fc3a65c84c
彭江勇 <530221078@qq.co>
2022-05-30 17:32:27 +0800

    优化

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { viteMockServe } from 'vite-plugin-mock'
 import path from 'path'
 
 export default ({ command, mode }) => {
