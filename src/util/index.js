b4731022873fc51ee9ba3c443cdcd43af181214d
pengjiangyong <15366181328@189.cn>
2022-06-07 15:15:57 +0800

    1

--- src/util/index.js
+++ src/util/index.js
@@ -1,8 +1,14 @@
 export default {
     //获取cookie
     getCookie(key) {
-        let result = document.cookie.match(new RegExp(`(?:^|\s)${key}=([^;]*)`));
-        return result && result[1]
+        let arr,
+            reg = new RegExp("(^| )" + key + "=([^;]*)(;|$)"); //匹配字段
+        arr = document.cookie.match(reg);
+        if (arr) {
+            return arr[2];
+        } else {
+            return null;
+        }
 
     },
     //添加
